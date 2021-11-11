b59e36dc4a2400bc913d5b5343c8c062cf2753c4
Georgiev <gino.georgiev@gmail.com>
2021-11-11 17:41:52 +0100

    Added custom Editor Buttons for CalculateResult and ResetSurvey

--- Assets/Features/Survey/UILogic/SurveyController.cs
+++ Assets/Features/Survey/UILogic/SurveyController.cs
@@ -11,32 +11,37 @@ namespace Features.Survey.UILogic
 {
     public class SurveyController : MonoBehaviour
     {
-        [Header("For UI QuestItem generation")]
-        [Tooltip("SO")] [SerializeField] private Questions_SO questionsData;
+        [Header("For UI QuestItem generation")] [Tooltip("SO")] [SerializeField]
+        private Questions_SO questionsData;
+
         private IEnumerable<Question_SO> questions;
         [Tooltip("Prefab")] [SerializeField] private GameObject questionItem;
         [Tooltip("Ref")] [SerializeField] private RectTransform content;
 
-        [Header("For calculating the Result")]
-        [SerializeField] private bool autoUpdateResult;
+        [Header("For calculating the Result")] [SerializeField]
+        private bool autoUpdateResult;
+
         [Tooltip("SO")] [SerializeField] private QuestionItemRuntimeSet questionItemRuntimeSet;
 
-        [Header("The 5 Aspects")]
-        [Tooltip("SO")] [SerializeField] private IntVariable extraversion;
+        [Header("The 5 Aspects")] [Tooltip("SO")] [SerializeField]
+        private IntVariable extraversion;
+
         [Tooltip("SO")] [SerializeField] private IntVariable agreeableness;
         [Tooltip("SO")] [SerializeField] private IntVariable conscientiousness;
         [Tooltip("SO")] [SerializeField] private IntVariable neuroticism;
         [Tooltip("SO")] [SerializeField] private IntVariable openness;
-        
-        [Header("The 5 Results for each Aspects")]
-        [Tooltip("Ref")] [SerializeField] private TMP_Text extraversionText;
+
+        [Header("The 5 Results for each Aspects")] [Tooltip("Ref")] [SerializeField]
+        private TMP_Text extraversionText;
+
         [Tooltip("Ref")] [SerializeField] private TMP_Text agreeablenessText;
         [Tooltip("Ref")] [SerializeField] private TMP_Text conscientiousnessText;
         [Tooltip("Ref")] [SerializeField] private TMP_Text neuroticismText;
         [Tooltip("Ref")] [SerializeField] private TMP_Text opennessText;
-        
-        [Header("The 5 Results for each Aspects in percent")]
-        [Tooltip("Ref")] [SerializeField] private TMP_Text extraversionPercentage;
+
+        [Header("The 5 Results for each Aspects in percent")] [Tooltip("Ref")] [SerializeField]
+        private TMP_Text extraversionPercentage;
+
         [Tooltip("Ref")] [SerializeField] private TMP_Text agreeablenessPercentage;
         [Tooltip("Ref")] [SerializeField] private TMP_Text conscientiousnessPercentage;
         [Tooltip("Ref")] [SerializeField] private TMP_Text neuroticismPercentage;
@@ -46,14 +51,14 @@ namespace Features.Survey.UILogic
         {
             // Cache Question List
             questions = questionsData.Items.OrderBy(x => Guid.NewGuid());
-            
+
             // Fill Scroll-View with Question Items and assign questions to those Items
             foreach (Question_SO question in questions)
             {
                 GameObject uiQuestionItem = Instantiate(questionItem, content);
                 uiQuestionItem.GetComponent<QuestionItemBehaviour>().Question = question;
             }
-            
+
             // Move the ResultItem to the last position of the Scroll-View
             content.GetChild(0).SetAsLastSibling();
         }
@@ -63,7 +68,7 @@ namespace Features.Survey.UILogic
             if (!autoUpdateResult) return;
             OnCalculateResult();
         }
-        
+
         public void OnCalculateResult()
         {
             // Restore values of each Aspect
@@ -72,27 +77,27 @@ namespace Features.Survey.UILogic
             conscientiousness.Restore();
             neuroticism.Restore();
             openness.Restore();
-            
+
             // Iterate over answers to sum up a result 
             foreach (QuestionItemBehaviour question in questionItemRuntimeSet.GetItems())
             {
-                foreach (Toggle t in question.Toggles)
+                foreach (Toggle toggle in question.Toggles)
                 {
                     // for positive keyed questions
                     if (question.Question.Key)
                     {
-                        switch (t.gameObject.name)
+                        switch (toggle.gameObject.name)
                         {
-                            case "Agree" when t.isOn:
+                            case "Agree" when toggle.isOn:
                                 question.Question.Aspect.Add(4);
                                 break;
-                            case "PartlyAgree" when t.isOn:
+                            case "PartlyAgree" when toggle.isOn:
                                 question.Question.Aspect.Add(3);
                                 break;
-                            case "Neither" when t.isOn:
+                            case "Neither" when toggle.isOn:
                                 question.Question.Aspect.Add(2);
                                 break;
-                            case "PartlyDisAgree" when t.isOn:
+                            case "PartlyDisAgree" when toggle.isOn:
                                 question.Question.Aspect.Add(1);
                                 break;
                         }
@@ -100,18 +105,18 @@ namespace Features.Survey.UILogic
                     // for negative keyed questions
                     else
                     {
-                        switch (t.gameObject.name)
+                        switch (toggle.gameObject.name)
                         {
-                            case "Disagree" when t.isOn:
+                            case "Disagree" when toggle.isOn:
                                 question.Question.Aspect.Add(4);
                                 break;
-                            case "PartlyDisagree" when t.isOn:
+                            case "PartlyDisagree" when toggle.isOn:
                                 question.Question.Aspect.Add(3);
                                 break;
-                            case "Neither" when t.isOn:
+                            case "Neither" when toggle.isOn:
                                 question.Question.Aspect.Add(2);
                                 break;
-                            case "PartlyAgree" when t.isOn:
+                            case "PartlyAgree" when toggle.isOn:
                                 question.Question.Aspect.Add(1);
                                 break;
                         }
@@ -137,6 +142,24 @@ namespace Features.Survey.UILogic
             conscientiousnessPercentage.text = conscientiousness.Get() * 2.5f + "%";
             neuroticismPercentage.text = neuroticism.Get() * 2.5f + "%";
             opennessPercentage.text = openness.Get() * 2.5f + "%";
+        }
+
+        public void ResetSurvey()
+        {
+            // Restore values of each Aspect
+            extraversion.Restore();
+            agreeableness.Restore();
+            conscientiousness.Restore();
+            neuroticism.Restore();
+            openness.Restore();
+
+            // Iterate over answers to reset toggles
+            foreach (Toggle toggle in questionItemRuntimeSet.GetItems().SelectMany(question => question.Toggles))
+            {
+                toggle.isOn = false;
+            }
+            
+            UpdateResultUI();
         }
     }
 }
