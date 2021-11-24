5297a02e071fc2293e7ec5f30678df3c75435784
kinasch <janik@schoenberg.name>
2021-11-24 20:03:11 +0100

    Added Teleport Method, Movement via Rigidbody

--- Assets/Features/Player/Logic/PlayerController.cs
+++ Assets/Features/Player/Logic/PlayerController.cs
@@ -6,6 +6,7 @@ using Features.Input;
 using Features.Player.Logic.States;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace Features.Player.Logic
@@ -17,6 +18,10 @@ namespace Features.Player.Logic
         [SerializeField] private Animator animator;
 
         [SerializeField] private FloatVariable playerMovementSpeed;
+
+        [SerializeField] private bool rigidbodyTestBool = false;
+
+        private new Rigidbody2D rigidbody2D;
 
         private PlayerControls playerControls;
         
@@ -34,6 +39,7 @@ namespace Features.Player.Logic
         {
             playerControls = new PlayerControls();
             stateMachine = new StateMachine();
+            rigidbody2D = this.GetComponent<Rigidbody2D>();
         }
         
         private void OnEnable()
@@ -72,29 +78,58 @@ namespace Features.Player.Logic
                 if(movementInput.x !=0) transform.localScale = new Vector3(Mathf.Sign(movementInput.x) * -1, 1, 1);
                 isWalking();
 
+                // Check for sprinting (holding down LSHIFT)
                 var movementSpeed = sprintInput > 0 ? sprintSpeed : walkingSpeed;
                 if (playerMovementSpeed.Get() != movementSpeed)
                 {
                     playerMovementSpeed.Set(movementSpeed);
                 }
+
+                if (rigidbodyTestBool)
+                {
+                    var xMovement = (transform.right * movementInput.x * playerMovementSpeed.Get());
+                    var yMovement = (transform.up  * movementInput.y * playerMovementSpeed.Get());
+                    // Temporary solution, objects with rigidbodies should use it's rigidbody
+                    rigidbody2D.velocity = xMovement + yMovement;
+                }
+                else
+                {
+                    var xMovement = (transform.right * Time.deltaTime * movementInput.x * playerMovementSpeed.Get());
+                    var yMovement = (transform.up * Time.deltaTime * movementInput.y * playerMovementSpeed.Get());
+                    // Temporary solution, objects with rigidbodies should use it's rigidbody
+                    transform.position += xMovement + yMovement;
+                }
                 
-                var xMovement = (transform.right * Time.deltaTime * movementInput.x * playerMovementSpeed.Get());
-                var yMovement = (transform.up * Time.deltaTime * movementInput.y * playerMovementSpeed.Get());
-                transform.position += xMovement + yMovement;
             }
             else
             {
+                // Set the state to idle while not walking and only if not idle already
                 if ( stateMachine.CurrentState != idleState)
                 {
                     stateMachine.ChangeState(idleState);
                     playerMovementSpeed.Set(0f);
                 }
+
+                if (rigidbodyTestBool)
+                {
+                    rigidbody2D.velocity = Vector2.zero;
+                }
             }
         }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            // TODO: Pick up algorithm (depends on the item)
+            Debug.Log(other);
+            
+            other.gameObject.SetActive(false);
+        }
+
 
         // For the GameEvent when the playerMovementSpeed changes
         public void ChangePlayerMovementSpeed()
         {
+            // Check the movement speed and adjust the state accordingly
             if (playerMovementSpeed.Get() == walkingSpeed)
             {
                 stateMachine.ChangeState(walkingState);
@@ -106,9 +141,15 @@ namespace Features.Player.Logic
             }
         }
 
-        private void OnTriggerEnter(Collider other)
+        // Teleport function
+        public void TeleportPlayer(Vector2 targetCoords, string targetSceneName=null)
         {
-            // Pick up stuff idk
+            if (targetSceneName != null)
+            {
+                SceneManager.SetActiveScene(SceneManager.GetSceneByName(targetSceneName));
+            }
+
+            transform.position = targetCoords;
         }
     }
 }
