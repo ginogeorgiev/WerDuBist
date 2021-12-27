fe989751e9cc3daf901bfb3be84cc97ba83331f9
Liz Kintzel <elisabeth.kintzel@student.htw-berlin.de>
2021-12-27 17:14:55 +0100

    58-implement-map-minimap:
    added PlayerPos to Map

---
+++ Assets/Features/Map/Logic/MapController.cs
@@ -0,0 +1,40 @@
+using DataStructures.Focus;
+using UnityEngine;
+using Features.Input;
+using Features.Quests.Logic;
+
+namespace Features.Map.Logic
+{
+    public class MapController : MonoBehaviour
+    {
+        [SerializeField] private GameObject mapUI;
+        
+        [SerializeField] private Transform player;
+        [SerializeField] private RectTransform playerIcon;
+        
+        [SerializeField] private Focus_SO<Quest_SO> questFocus;
+        
+        
+        public void ToggleMapUI()
+        {
+            mapUI.SetActive(!mapUI.activeSelf);
+        }
+
+        void Update()
+        {
+            if (mapUI.activeSelf)
+            {
+                playerIcon.position = WorldToMap(player.position);
+            }
+        }
+
+        private Vector3 WorldToMap(Vector3 worldCoordinates)
+        {
+            return new Vector3(
+                432 + (worldCoordinates.x / 30 * 530), 
+                243 + worldCoordinates.y / 21 * 369, 
+                worldCoordinates.z);
+        }
+
+    }
+}
