14bc5406ed18ca454cdae9df7f7c56e6f1a7eb5f
Liz Kintzel <elisabeth.kintzel@student.htw-berlin.de>
2022-01-10 11:20:04 +0100

    58-implement-map-minimap:
    added MainIsland Map
    fixxed Error on QuestCompleted

---
+++ Assets/Features/Map/Logic/MapIconController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Features.Map.Logic
+{
+    public class MapIconController  : MonoBehaviour
+    {
+        [SerializeField] private GameObject playerMarker;
+        
+        public void resizeIcon()
+        {
+            playerMarker.transform.localScale = new Vector3(8, 8, 1);
+        }
+    }
+}
